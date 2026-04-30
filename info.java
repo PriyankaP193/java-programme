b710b03d153b5ef92cd1cd02f592a6b759319038
PriyankaP193 <priyankapv08@gmail.com>
2026-04-30 14:47:46 +0530

    getting input from user and printing
    Signed-off-by: PriyankaP193 <priyankapv08@gmail.com>

---
+++ info.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class info{
+    public static void main(String[] args){
+        Scanner r = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = r.next();
+        System.out.println(name);
+        System.out.println("Enter your age: ");
+        int age = r.nextInt();
+        System.out.println(age);
+        r.nextLine();
+        System.out.println("Enter your college: ");
+        String college = r.nextLine();
+        System.out.println(college);
+        System.out.println("Enter your kecmail: ");
+        String mail=r.next();
+        System.out.println(mail);
+        System.out.println("Enter your cgpa: ");
+        float marks=r.nextFloat();
+        System.out.println(marks);
+        System.out.println("Enter a ph.no: ");
+        long num=r.nextLong();
+        System.out.println(num);
+        
+
+
+
+
+    }
+}
